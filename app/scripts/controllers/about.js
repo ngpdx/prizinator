3a47cb80f9d92131171326827bf69eefb0210486
Hensley, Michael (DS) <Michael.Hensley@cdk.com>
2015-04-08 18:10:01 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPrizinatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularPrizinatorApp
+ */
+angular.module('angularPrizinatorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPrizinatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularPrizinatorApp
+ */
+angular.module('angularPrizinatorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
